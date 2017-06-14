e14d9b6c1534de819f733d4de367352fe1c08d45
Oksana Perun <oksanaperun@users.noreply.github.com>
2017-06-14 23:39:36 +0300

    IP and port config

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var restify = require('restify'),
   mongojs = require('mongojs'),
+  port = process.env.Port || 8080,
+  ip = process.env.IP || '0.0.0.0',
   db = mongojs(process.env.MongoConnectionString, ['wordsinaword']),
   server = restify.createServer();
  
@@ -7,8 +9,8 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.listen(3000, function () {
-    console.log('Server started @ 3000');
+server.listen(port, ip, function () {
+    console.log('Server started at ' + ip + ':' + port);
 });
 
 server.get('/wordsinaword', function (req, res, next) {
